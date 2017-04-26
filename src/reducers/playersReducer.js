450175814049186de25b3e3dc75b8815a20ed151
Tobias Timm <t.timm@mediaform.de>
2017-04-26 14:59:01 +0200

    - fix in restart (duplicated players)

--- src/reducers/playersReducer.js
+++ src/reducers/playersReducer.js
@@ -3,7 +3,7 @@ import {RESET_TURNS} from '../actions/PlayersActionTypes';
 const playersReducer = function playersReducer(state = [], action) {
   switch (action.type) {
     case RESET_TURNS:
-      return [...state, ...state.map((player) => {
+      return [...state.splice(0,...state.length), ...state.map((player) => {
         return {...player, turns: 0};
       })];
     default:
